a1966ce3646cdddae8f85e2cbb69de13824ae78e
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2024-02-14 08:39:37 +0100

    Update new-container.js

--- assets/elements/new-container.js
+++ assets/elements/new-container.js
@@ -3,8 +3,8 @@ let containerIndex = 0; // Huidige index van de container
 // Inhoud van de containers
 const exercises = [
     { title: "Push-ups", description: "Push-ups zijn een effectieve lichaamsgewichtoefening waarbij je jezelf vanuit een plankpositie omhoog drukt en weer laat zakken. Deze oefening richt zich op de borstspieren, triceps en schouders, terwijl ook de kernspieren worden geactiveerd. Met 10 herhalingen stimuleer je kracht en uithoudingsvermogen.", oud: "21,34s", pr: "16,23s" },
-    { title: "Benchpress", description: "De bench press is een krachtoefening waarbij een persoon liggend op een speciale bank een halter boven de borst laat zakken en vervolgens weer omhoog duwt. Het is een essentiële oefening in krachttraining en bodybuilding, gericht op het ontwikkelen van de borstspieren, triceps en schouders.", oud: "30kg", pr: "42.5kg" },
-    { title: "10km Hardlopen", description: "De 10 kilometer hardloopwedstrijd is een populaire afstand waarbij hardlopers hun uithoudingsvermogen en snelheid testen. De afstand vereist een goede combinatie van duurvermogen en snelheid, waardoor het een geliefde discipline is binnen de hardloopgemeenschap.", oud: "1 uur en 20 minuten", pr: "55:58" },
+    { title: "Benchpress", description: "De bench press is een krachtoefening waarbij een persoon liggend op een speciale bank een halter boven de borst laat zakken en vervolgens weer omhoog duwt. Het is een essentiële oefening in krachttraining en bodybuilding, gericht op het ontwikkelen van de borstspieren, triceps en schouders.", oud: "42.5kg", pr: "50kg" },
+    { title: "10km Hardlopen", description: "De 10 kilometer hardloopwedstrijd is een populaire afstand waarbij hardlopers hun uithoudingsvermogen en snelheid testen. De afstand vereist een goede combinatie van duurvermogen en snelheid, waardoor het een geliefde discipline is binnen de hardloopgemeenschap.", oud: "1 uur en 20 minuten", pr: "43:21" },
     // Voeg hier eventueel meer oefeningen toe
 ];
 
